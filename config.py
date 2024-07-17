8d19293c7132883552f34ec1855c43a1fd99df6b
12Ddos <kumarkaran885342@gmail.com>
2024-07-17 17:52:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7103285870:AAFMRchdCpDfjfFsjaxBuu-XDiuSCZ5kn7w")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("karanarjun80")
+OWNER_USERNAME=("Karanarjun80")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1609642320")
