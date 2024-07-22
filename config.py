f4c19c0ae5bb7c887a71ead1fce0f8a302fb5953
Bis2818 <vishalpanika922@gmail.com>
2024-07-22 15:44:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7460796825:AAHn42eaQJWd5UBoxc03E0-2agInw_hHkvE")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("@Vishalpanika")
 
   #Enter your admin id herehere Get it from @missRose_bot by typing /info
-ADMIN_IDS=("55889")
+ADMIN_IDS=("5783460883")
 9")
 herehere
 
